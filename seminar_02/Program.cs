598d9a3322d1cc584bd319ed0ca703f665678662
Salavat <s.faskhutdinov@inbox.ru>
2022-08-25 00:05:33 +0300

    add new changes 1

--- seminar_02/Program.cs
+++ seminar_02/Program.cs
@@ -1,11 +1,62 @@
 ﻿//11 - задача. Напишит епрограмму, которая выдает случайное число и удаляет вторую цифру этого числа.
 
-Console.Clear();
+//Console.Clear();
 
-int num = new Random(). Next (100, 1000);
-Console.WriteLine(num);
-
-
-Console.WriteLine( num / 100 * 10 + num % 10);
+//int num = new Random(). Next (100, 1000);
+//Console.WriteLine(num);
 
 
+//Console.WriteLine( num / 100 * 10 + num % 10);
+
+
+
+
+// 16 - задача. Гапишит епрограмму, которая на вход принимает 2 числа, а на выходе проверяет является ли одно число квадратом другого.
+
+
+//Console.WriteLine(" Введите 2 числа: ");
+
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//int num2 = Convert.ToInt32(Console.ReadLine());
+
+//if( num2 == Math.Pow(num1, 2 ))
+//{
+  //  Console.WriteLine(" Да");
+//} 
+//else 
+//{
+  //  Console.WriteLine(" Нет");
+//}
+
+// 12 - задача. Напишите программу , которая на вход принимает 2 числла, а на выходе проверяет является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+
+//Console.WriteLine(" Введите 2 числа: ");
+
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//int num2 = Convert.ToInt32(Console.ReadLine());
+
+//if (num1 % num2 == 0)
+//{
+    //Console.WriteLine(" Кратно");
+//}
+//else 
+//{
+   // Console.WriteLine(" Не кратно, остаток = " + num1 % num2);
+//}
+
+
+//14 - задача. Напишите программу , которая принимает на вход число и проверяет  кратно ли оно одновременно 23 и 7. 
+
+//Console.WriteLine(" Введите чсило: ");
+
+//int num = Convert.ToInt32(Console.ReadLine());
+
+//if (num % 7 == 0 & num % 23 == 0)
+//{
+   // Console.WriteLine("Да");
+//}
+//else
+//{
+   // Console.WriteLine("Нет");
+//}
